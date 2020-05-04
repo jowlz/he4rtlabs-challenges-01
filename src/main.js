4261dd101aaa9e6dad863a9b8e7f8834af7110b7
Joao Luiz Neto Silveira <jotabtw@gmail.com>
2020-05-04 12:35:07 -0300

    Ajustes JS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 function calcular(){
-    var tempoDiario = document.getElementById('tempoDiario');
-    var diasTrabalhados = document.getElementById('diasTrabalhados');
-    var ferias = document.getElementById('ferias');
-    var custoProjeto = document.getElementById('custoTotal');
-    
-    valorHora = (custoProjeto.value / (diasTrabalhados.value * 4 * tempoDiario.value) ) + ( ( ferias.value * diasTrabalhados.value * tempoDiario.value ) )
+    var horasDiarias = document.getElementById('horasDiarias');
+    var diasEfetivos = document.getElementById('diasEfetivos');
+    var diasFerias = document.getElementById('diasFerias');
+    var valorProjeto = document.getElementById('valorProjeto');
+
+    var valorHora = (valorProjeto.value / (diasEfetivos.value * 4 * horasDiarias.value) ) + ( ( diasFerias.value * diasEfetivos.value * horasDiarias.value ) )
+
+    document.getElementById('resultado').innerHTML = `Sua hora vale em média R$${parseFloat(valorHora).toFixed(2)} !`;
 };
